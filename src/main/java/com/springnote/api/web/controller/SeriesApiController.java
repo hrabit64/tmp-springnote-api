b99499728921ab3d5c75f0907dc0f7736d9dfc8c
Junser Hwang <hzser123@gmail.com>
2023-10-27 18:13:31 +0900

    [FIX] CORS 에러 수정

--- src/main/java/com/springnote/api/web/controller/SeriesApiController.java
+++ src/main/java/com/springnote/api/web/controller/SeriesApiController.java
@@ -22,7 +22,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "")
+
 @Validated
 @Slf4j
 @RequiredArgsConstructor
